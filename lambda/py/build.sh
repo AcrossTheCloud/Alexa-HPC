8805d7cd6bb6915f45724b29a571877c184dbbcb
Matthew Berryman <matthew@acrossthecloud.net>
2019-03-29 12:33:27 +1100

    script for building and zipping everything including cross-compilation

---
+++ lambda/py/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm build.zip
+mkdir -p build
+docker run --rm -v $(pwd):/var/task lambci/lambda:build-python3.7 python3.7 -m pip install -t /var/task/build -r requirements.txt
+cp -r .parallelcluster parallel_cluster.py build
+mv build/bin/pcluster build/pcluster-cli
+cd build
+zip -r -9 ../build.zip * .libs_cffi_backend .parallelcluster
+cd ..
+rm -r build
