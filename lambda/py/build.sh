1a37932602ce3de897fbf5f7fc87e4886e248b54
Matthew Berryman <matthew@acrossthecloud.net>
2019-11-02 13:51:43 +1100

    updates for aws-parallelcluster 2.4.1

--- lambda/py/build.sh
+++ lambda/py/build.sh
@@ -5,6 +5,7 @@ docker run --rm -v $(pwd):/var/task lambci/lambda:build-python3.7 python3.7 -m p
 cp -r .parallelcluster parallel_cluster.py build
 mv build/bin/pcluster build/pcluster-cli
 cd build
+rm -rf enum enum34*
 zip -r -9 ../build.zip * .libs_cffi_backend .parallelcluster
 cd ..
 rm -r build
